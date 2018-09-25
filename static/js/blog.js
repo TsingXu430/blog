f41733b962956d8189537e5f6f39d6da61c35a89
tsingxu430 <734536013@qq.com>
2018-09-25 15:23:26 +0800

    fix

--- static/js/blog.js
+++ static/js/blog.js
@@ -165,7 +165,8 @@ blog.addLoadEvent(function () {
 });
 
 //主题标识
+/*
 blog.addLoadEvent(function () {
     console.info("Author Blog %chttp://blog.tmaize.net", "color: #FF0000;");
     console.info("Github Site %chttps://github.com/TMaize/tmaize-blog", "color: #FF0000;");
-});
+});*/
